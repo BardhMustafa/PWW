68495e51b3592cd1e6b79cce9c55acf38d23e91a
LauraRrahmani <56886059+LauraRrahmani@users.noreply.github.com>
2022-12-27 15:05:22 +0100

    Update setLocalStorage.js
    functions to set and get data on/from local storage

--- js/setLocalStorage.js
+++ js/setLocalStorage.js
@@ -1,5 +1,9 @@
 const KEY = "PWW";
 
 function setDataInLS(path, value) {
+    window.localStorage.setItem(KEY + path, JSON.stringify(value));
+}
 
+function getDataFromLS(path) {
+    return window.localStorage.getItem(KEY + path)
 }
